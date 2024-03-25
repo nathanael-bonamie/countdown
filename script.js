e92b301ea72a07e4ba66433605593a8b02ccd6de
Nathanael Bonamie <80281444+nathanael-bonamie@users.noreply.github.com>
2024-03-25 20:23:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -39,34 +39,8 @@ var x = setInterval(function() {
 
   var distance = countDownDate - now;
   
-  // Output the result debut an element with id="demo"
-  if (pause){
-	affTime.innerHTML = "<span class='color-blue'>"+(h<10 ? "0":"")+h+":"+(m<10 ? "0":"")+m+":"+(s<10 ? "0":"")+ s + "</span>";
-	}
-  else if(dt.getDay()==1 || dt.getDay()==3 || dt.getDay()==5){
-	affTime.innerHTML = "<span class='color-blue'>"+(h<10 ? "0":"")+h+":"+(m<10 ? "0":"")+m+":"+(s<10 ? "0":"")+ s + "</span>";
-	}
-  else{
-	affTime.innerHTML = "<span class='color-blue'>"+(h<10 ? "0":"")+h+":"+(m<10 ? "0":"")+m+":"+(s<10 ? "0":"")+ s + "</span>";
-	}
-    
-  // affichage congés, chômage ou week end
-  if (week_end && sessionStorage.getItem("howStart")==1){
-  	affTime.innerHTML = "<span class='color-blue'>"+(h<10 ? "0":"")+h+":"+(m<10 ? "0":"")+m+":"+(s<10 ? "0":"")+s+"</span>";
-	affLogo.innerHTML = "<span class='color-green'>"+"Week-end"+"</span>";
-  }
-	//congés
-  else if (sessionStorage.getItem("howStart")==6){
-	affTime.innerHTML = "<span class='color-blue'>"+(h<10 ? "0":"")+h+":"+(m<10 ? "0":"")+m+":"+(s<10 ? "0":"")+s+"</span>";
-	affLogo.innerHTML = "<span class='color-green'>"+sessionStorage.getItem("txt")+"</span>";
-  }
-	//vacances
-  else if (sessionStorage.getItem("howStart")==7){
-	affTime.innerHTML = "<span class='color-blue'>"+(h<10 ? "0":"")+h+":"+(m<10 ? "0":"")+m+":"+(s<10 ? "0":"")+s+"</span>";
-	affLogo.innerHTML = "<span class='color-green'>"+sessionStorage.getItem("txt").substring(0, 11)
-	+"<br>"+sessionStorage.getItem("txt").substring(11)+"</span>";
-  }
-  
+  affTime.innerHTML = "<span class='color-blue'>"+(h<10 ? "0":"")+h+":"+(m<10 ? "0":"")+m+":"+(s<10 ? "0":"")+ s + "</span>";
+
   if(distance < 0){
 	setcount();
   }
@@ -78,37 +52,6 @@ function playAudio(url) {
 }
 
 function setcount(){
-
-d=parseInt(new Date().getHours()+""+(10>new Date().getMinutes() ? "0" : "")+new Date().getMinutes());
-if (d<900){
-	pause=true;
-    dt = new Date();
-	dt.setHours(09);
-	dt.setMinutes(00);
-	dt.setSeconds(00);
-	countDownDate=dt.getTime();
-	affLogo.innerHTML = "<span><a href='tel:0679929758' style='color:green'><img width='64' height='64' src='./phone.png'/></a></span>";
-	}
-else if (d>=900 && d<1600){
-	pause=false;
-    dt = new Date();
-	dt.setHours(16);
-	dt.setMinutes(00);
-	dt.setSeconds(00);
-	countDownDate=dt.getTime();
-	if(dt.getDay()==1 || dt.getDay()==3 || dt.getDay()==5)
-		affLogo.innerHTML = "<span><a href='tel:0553879932' style='color:green'><img width='64' height='64' src='./phone.png'/></a></span>";
-	else
-		affLogo.innerHTML = "<span><a href='tel:0553958939' style='color:green'><img width='64' height='64' src='./phone.png'/></a></span>";
-	}
-else{
-	pause=true;
-	dt = new Date();
-	countDownDate=0;
-	end=true;
-	affLogo.innerHTML = "<span><a href='tel:0679929758' style='color:green'><img width='64' height='64' src='./phone.png'/></a></span>";
-	}
-	
 let xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
@@ -141,4 +84,44 @@ xmlhttp.open(
   "https://api.github.com/repos/nathanael-bonamie/countdown/contents/how.txt",
   false);
 xmlhttp.send();
+
+d=parseInt(new Date().getHours()+""+(10>new Date().getMinutes() ? "0" : "")+new Date().getMinutes());
+if(!week_end && sessionStorage.getItem("howStart")==1){
+	if (d<900){
+		pause=true;
+	    dt = new Date();
+		dt.setHours(09);
+		dt.setMinutes(00);
+		dt.setSeconds(00);
+		countDownDate=dt.getTime();
+		affLogo.innerHTML = "<span><a href='tel:0679929758' style='color:green'><img width='64' height='64' src='./phone.png'/></a></span>";
+		}
+	else if (d>=900 && d<1600){
+		pause=false;
+	    dt = new Date();
+		dt.setHours(16);
+		dt.setMinutes(00);
+		dt.setSeconds(00);
+		countDownDate=dt.getTime();
+		if(dt.getDay()==1 || dt.getDay()==3 || dt.getDay()==5)
+			affLogo.innerHTML = "<span><a href='tel:0553879932' style='color:green'><img width='64' height='64' src='./phone.png'/></a></span>";
+		else
+			affLogo.innerHTML = "<span><a href='tel:0553958939' style='color:green'><img width='64' height='64' src='./phone.png'/></a></span>";
+		}
+	else{
+		pause=true;
+		dt = new Date();
+		countDownDate=0;
+		end=true;
+		affLogo.innerHTML = "<span><a href='tel:0679929758' style='color:green'><img width='64' height='64' src='./phone.png'/></a></span>";
+		}
+	}
+else if (week_end && sessionStorage.getItem("howStart")==1){
+	affLogo.innerHTML = "<span class='color-green'>"+"Week-end"+"</span>";}
+else if (sessionStorage.getItem("howStart")==6){
+	affLogo.innerHTML = "<span class='color-green'>"+sessionStorage.getItem("txt")+"</span>";
+}
+else if (sessionStorage.getItem("howStart")==7){
+	affLogo.innerHTML = "<span class='color-green'>"+sessionStorage.getItem("txt").substring(0, 11)
+	+"<br>"+sessionStorage.getItem("txt").substring(11)+"</span>";}
 }
